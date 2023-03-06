5905a2ca8fb5bc8d53cb3326fcf0f127cf69e112
kimjihun <jihoon_story@naver.com>
2023-03-06 16:33:24 +0900

    Init: 프로젝트 생성

---
+++ AOS/app/src/main/java/com/edu/mf/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.edu.mf.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edu.mf.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
