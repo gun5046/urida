0f5d7d315a5571a91b25c2218cfc7313fdd7ed3e
장보경 <jjjjyj4870@naver.com>
2023-03-08 13:34:55 +0900

    Feat: 화면이동 카드뷰 생성 Related to: #106

---
+++ AOS/app/src/main/java/com/edu/mf/view/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.edu.mf.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.edu.mf.databinding.FragmentMainBinding
+import com.edu.mf.view.common.MainActivity
+import com.edu.mf.view.drawing.DrawingFragment
+import com.edu.mf.view.picture.PictureFragment
+import com.edu.mf.view.study.StudyFragment
+
+class MainFragment: Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        mainActivity = MainActivity.getInstance()!!
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            cardviewStudy.setOnClickListener {
+                mainActivity.addFragment(StudyFragment())
+            }
+
+            cardviewDrawing.setOnClickListener {
+                mainActivity.addFragment(DrawingFragment())
+            }
+
+            cardviewPicture.setOnClickListener {
+                mainActivity.addFragment(PictureFragment())
+            }
+        }
+    }
+}
---
+++ AOS/app/src/main/java/com/edu/mf/view/study/StudyFragment.kt
@@ -0,0 +1,45 @@
+package com.edu.mf.view.study
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.edu.mf.databinding.FragmentStudyBinding
+import com.edu.mf.view.common.MainActivity
+import com.edu.mf.view.study.learn.LearnFragment
+import com.edu.mf.view.study.quiz.QuizFragment
+import com.edu.mf.view.study.reslove.ResolveFragment
+
+class StudyFragment: Fragment() {
+    private lateinit var binding: FragmentStudyBinding
+    private lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStudyBinding.inflate(inflater, container, false)
+        mainActivity = MainActivity.getInstance()!!
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            cardviewLearn.setOnClickListener {
+                mainActivity.addFragment(LearnFragment())
+            }
+
+            cardviewQuiz.setOnClickListener {
+                mainActivity.addFragment(QuizFragment())
+            }
+
+            cardviewResolve.setOnClickListener {
+                mainActivity.addFragment(ResolveFragment())
+            }
+        }
+    }
+}
