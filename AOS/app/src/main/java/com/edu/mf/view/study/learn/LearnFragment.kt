a79861aed52e0d1f0f51d01b0e34d8738c485b56
kimjihun <jihoon_story@naver.com>
2023-03-10 01:53:19 +0900

    Feat: 카테고리 선택 페이지 작업중 #90

--- AOS/app/src/main/java/com/edu/mf/view/study/learn/LearnFragment.kt
+++ AOS/app/src/main/java/com/edu/mf/view/study/learn/LearnFragment.kt
@@ -4,22 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.edu.mf.R
 import com.edu.mf.databinding.FragmentLearnBinding
 
+
 class LearnFragment: Fragment() {
-    private lateinit var binding: FragmentLearnBinding
+    private var categories = arrayListOf<String>()
+    private lateinit var learnAdapter: LearnAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLearnBinding.inflate(inflater, container, false)
+        val binding:FragmentLearnBinding =
+            DataBindingUtil.inflate(inflater,R.layout.fragment_learn,container,false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    /**
+     * 초기화 데이터
+     */
+    private fun init(){
+        setCategoryData()
+    }
+    /**
+     * category 데이터 입력
+     */
+    private fun setCategoryData(){
+        categories.add("과일/채소")
+        categories.add("직업")
+        categories.add("동물")
+        categories.add("물체")
+        categories.add("장소")
+        categories.add("행동")
+    }
+
+    /**
+     * adapter 초기화
+     */
+    private fun setAdapter(){
+        learnAdapter.setList(categories)
+        binding
     }
 }
