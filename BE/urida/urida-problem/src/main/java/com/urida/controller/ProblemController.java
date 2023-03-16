86aceb89649ab1a9b0679766200304a8c303d510
luminaries1 <luminaries1@naver.com>
2023-03-16 13:50:25 +0900

    Feat: [BE] 스웨거 작업 추가 #31

--- BE/urida/urida-problem/src/main/java/com/urida/controller/ProblemController.java
+++ BE/urida/urida-problem/src/main/java/com/urida/controller/ProblemController.java
@@ -4,7 +4,7 @@ import com.urida.dto.ProblemSaveDto;
 import com.urida.impl.ProblemService;
 import com.urida.entity.Problem;
 import com.urida.exception.InputException;
-import lombok.Getter;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +23,13 @@ public class ProblemController {
     Logger logger = LoggerFactory.getLogger(ProblemController.class);
 
     @GetMapping("/info")
+    @ApiOperation(value = "문제정보", notes = "문제 Id / 해당 문제 없음 -> 빈 Problem 객체 리턴/ Input 값오류->403 error")
     public Problem getProblemInfo(@RequestParam Long proId){
         return problemService.problemInfo(proId);
     }
 
     @PostMapping("/save")
+    @ApiOperation(value = "문제저장", notes = " 카테고리 Id, 빈칸 문장의 경우 빈칸 문장 Id, 문제 타입, 답 Id, 틀린 횟수, userid/ ")
     public Boolean saveProblem(@Validated @RequestBody ProblemSaveDto problemSaveDto, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new InputException("RequestData(RegisterDto))invalid");
@@ -39,11 +41,13 @@ public class ProblemController {
     }
 
     @GetMapping("/list")
+    @ApiOperation(value = "해당유저의 틀린 문제 리스트", notes = "UserId 필요")
     public List<Problem> getListProblem(@RequestParam Long userId){
         return problemService.getListProblem(userId);
     }
 
     @DeleteMapping("/delete")
+    @ApiOperation(value = "리스트에서 문제 삭제", notes = "UserId 와 ProId 필요 (특정 유저의 문제 리스트에서 특정 문제 삭제)")
     public void deleteProblem(@RequestParam Long userId, @RequestParam Long proId){
         problemService.deleteProblem(userId, proId);
     }
