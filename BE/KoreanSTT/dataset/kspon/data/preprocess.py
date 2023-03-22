ed0bf30f904c2f8622a3b649cc8a99eb87f92f1b
gun5046@naver.com <gun5046@naver.com>
2023-03-22 10:45:03 +0900

    Feat: STT kospeech 추가

---
+++ BE/KoreanSTT/dataset/kspon/data/preprocess.py
@@ -0,0 +1,26 @@
+import os
+
+
+def stringParser() :
+    change = ['.','!','?',',','\'']
+
+    file = open("./dataset/kspon/data/transcript.v.1.4.txt",encoding='UTF8')
+    audio_path = []
+    transcripts = []
+    while True : 
+        line = file.readline()
+
+        string = line.split('|')
+        if not line : break
+        # print(string[1])
+        # dict[string[0]]=string[1]
+        audio_path.append(string[0].replace('/','\\'))
+
+        for idx in range(5) :
+            string[2] = string[2].replace(change[idx],'')
+            
+        
+        transcripts.append(string[2])
+
+    file.close()
+    return audio_path,transcripts
