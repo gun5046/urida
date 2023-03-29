4e219cdc62f91ec383874b81fe521586c25b8808
jkjk0903 <jkjk0903@gmail.com>
2023-03-29 16:18:11 +0900

    Chore:[BE] controller 변경

--- BE/urida/urida-board/src/main/java/com/urida/board/controller/BoardController.java
+++ BE/urida/urida-board/src/main/java/com/urida/board/controller/BoardController.java
@@ -46,7 +46,7 @@ public class BoardController {
             @ApiResponse(responseCode = "404", description = "사용자 없음"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
-    public Board createArticle(@Validated @ModelAttribute ArticleCreateDto articleCreateDto, BindingResult bindingResult) {
+    public Board createArticle(@Validated @RequestBody ArticleCreateDto articleCreateDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new InputException("RequestData(ArticleDto)invalid");
         }
